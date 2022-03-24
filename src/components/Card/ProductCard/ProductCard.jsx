9c6b4066d6dd315c059b19de6406592753ca8e9e
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-03-24 17:47:17 +0530

    Feat: Add ldynamic link to products

--- src/components/Card/ProductCard/ProductCard.jsx
+++ src/components/Card/ProductCard/ProductCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import "./product-card.css"
 
 function ProductCard({info, EMI}) {
-  const {title, price, src} = info.prod;
+  const {title, price, src, _id} = info.prod;
   const {display, computable}=  price;  
   const calcEMI= Math.round(computable/EMI);
   return (
     <div className="card  flex-column">
+      <Link to={`/product/${_id}`}>
                 <div className="img-container">
                     <img className="prod-img" alt="Product Image"
                         src={src}/>
@@ -16,6 +18,7 @@ function ProductCard({info, EMI}) {
                     <p className="prod-cost">{display}</p>
                     <p className="para-s gray-text">EMI starts at ₹{calcEMI}/month</p>
                 </div>
+                </Link>
                 <div className="card-btns flex-row">
                     <button className="btn prim-acc-btn">Bookmark</button>
                     <button className="btn prim-btn">Book Now</button>
