065598286a8a59cb5dc86d36854064ebe5d74872
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-03-25 22:59:57 +0530

    Feat: Add Bookmark

--- src/components/Card/ProductCard/ProductCard.jsx
+++ src/components/Card/ProductCard/ProductCard.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
 import "./product-card.css"
+import {useAuth} from "../../../contexts/Providers/AuthProvider/AuthProvider";
+import axios from "axios";
+import { useBookmarks } from '../../../contexts/Providers/BookmarksProvider/BookmarksProvider';
+import {useLoading} from "../../../hooks/useLoading";
 
 function ProductCard({info, EMI}) {
-  const {title, price, src} = info.prod;
+  const {authState} = useAuth();
+  const {bookmarksState, bookmarksDispatch} = useBookmarks();
+  const {loading:bookmarkLoading, setLoading:bookmarkSetLoading} = useLoading();
+  const {title, price, src, _id} = info.prod;
   const {display, computable}=  price;  
   const calcEMI= Math.round(computable/EMI);
+
+  const {bookmarks} = bookmarksState;
+
+  const addToBookmarks= async (product)=>{
+   if(!authState.isAuthenticated){
+     alert("Need to login first");
+     return;
+   }
+   bookmarkSetLoading(true);
+     try{
+       const token=localStorage.getItem("token");
+       const response= await axios({
+       method:"post",
+       url:"/api/user/wishlist",
+       headers:{authorization:token},
+       data:{product:product}
+    });
+    bookmarkSetLoading(false);
+    bookmarksDispatch ({type:"ADD_TO_BOOKMARKS", payload:response.data.wishlist});
+}catch(e){
+    console.log(e);
+    bookmarkSetLoading(false);
+}
+    }
+
+const removeFromBookmarks=async(id)=>{
+  bookmarkSetLoading(true);
+    try{
+     const response= await axios({
+         method:"DELETE",
+         url:`/api/user/wishlist/${id}`,
+         headers:{authorization:localStorage.getItem("token")}
+     });
+     bookmarkSetLoading(false);
+     bookmarksDispatch({type:"REMOVE_FROM_BOOKMARKS", payload:response.data.wishlist})
+    }catch(e){
+        console.log(e)
+        bookmarkSetLoading(false);
+    }
+
+  }
+
+const bookmarkBtn= bookmarks.some(bookmark=> bookmark._id===_id) ? 
+<button onClick={()=> removeFromBookmarks(_id)} className="btn prim-acc-btn">{bookmarkLoading?<i className="fa fa-spinner fa-spin"/>:"Bookmarked"}</button>:
+<button onClick={()=> addToBookmarks(info.prod)} className="btn prim-acc-btn">{bookmarkLoading?<i className="fa fa-spinner fa-spin"/>:"Bookmark"}</button> ;
+
   return (
     <div className="card  flex-column">
                 <div className="img-container">
@@ -14,10 +68,10 @@ function ProductCard({info, EMI}) {
                 <div className="text-container flex-column">
                     <p className="prod-title">{title}</p>
                     <p className="prod-cost">{display}</p>
-                    <p className="para-s gray-text">EMI starts at ₹{calcEMI}/month</p>
+                    <p className="para-s gray-text">EMI starts at ₹{calcEMI.toLocaleString()}/month</p>
                 </div>
                 <div className="card-btns flex-row">
-                    <button className="btn prim-acc-btn">Bookmark</button>
+                    {bookmarkBtn}
                     <button className="btn prim-btn">Book Now</button>
                 </div>
                 </div>
--- src/pages/Bookmark/Bookmark.jsx
+++ src/pages/Bookmark/Bookmark.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import "./bookmark.css";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link} from 'react-router-dom';
+import BookmarkCard from '../../components/Card/BookmarkCard/BookmarkCard';
+import {useDocumentTitle} from "../../hooks/useDocumentTitle";
+import { useBookmarks } from '../../contexts/Providers/BookmarksProvider/BookmarksProvider';
 
 function Bookmark() {
+const token = localStorage.getItem("token");
+const {bookmarksState, bookmarksDispatch} = useBookmarks();
+const {bookmarks} = bookmarksState; 
+
+
+useDocumentTitle("Bookmarks | CarSmart");
+
+useEffect(()=>{
+
+    (async ()=>{
+        try{
+            const response=await axios({
+                method:"get",
+                url:"/api/user/wishlist",
+                headers:{authorization: token}
+            });
+            bookmarksDispatch({type:"FETCH_BOOKMARKS", payload:response.data.wishlist});
+
+        }catch(e){
+            console.log(e);
+        }
+
+    })();
+
+},[]);
+
+const items = 
+bookmarks.length===0? <main className='bookmark-main flex-column'>
+    <h3>Oh no! seems like you haven't bookmarked anything.</h3>
+    <h4>Bookmark Items To View Them Here</h4>
+    <Link className='btn prim-btn' to={"/products"}>View Products</Link>
+        </main> : <div className='bookmark-grid grid auto-grid'>
+       {bookmarks.map(bookmark=> <BookmarkCard key={bookmark._id} prod={bookmark}/>)}
+         </div>;
+
   return (
-    <div>Bookmark</div>
+    <main className='bookmark-main flex-column'>
+        <h3 className="bookmark-heading">Your Bookmarks</h3>
+           {items}
+    </main>
   )
 }
 
