94a72310b08e2ed3ccc591be92085f530e4905e9
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-03-26 11:42:59 +0530

    Feat: Add bookmark button component

---
+++ src/components/BookmarkButton/BookmarkButton.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {useLoading} from "../../hooks/useLoading";
+import {useAuth} from "../../contexts/Providers/AuthProvider/AuthProvider";
+import {useBookmarks} from "../../contexts/Providers/BookmarksProvider/BookmarksProvider";
+import {removeFromBookmarks, addToBookmarks} from "../../contexts/Providers/BookmarksProvider/helpers";
+
+
+function BookmarkButton({prod, classes}) {
+const {authState} = useAuth();
+const {loading:bookmarkLoading, setLoading:setBookmarkLoading} = useLoading();
+const {bookmarksState, bookmarksDispatch} = useBookmarks();
+const {bookmarks} = bookmarksState;
+
+
+const addBookmarkHandler= (product)=>{
+    if(!authState.isAuthenticated){
+      alert("Need to login first");
+      return;
+    }
+     const token=localStorage.getItem("token");
+     addToBookmarks(product, token, setBookmarkLoading, bookmarksDispatch);
+     }
+
+const removeBookmarkHandler=(id)=>{
+        const token= localStorage.getItem("token");
+        removeFromBookmarks(id, token, setBookmarkLoading, bookmarksDispatch);
+  }
+
+
+const buttonElement= bookmarks.some(bookmark=> bookmark._id===prod._id) ? 
+<button onClick={()=> removeBookmarkHandler(prod._id)} className={classes}>{bookmarkLoading?<i className="fa fa-spinner fa-spin"/>:"Bookmarked"}</button>:
+<button onClick={()=> addBookmarkHandler(prod)} className={classes}>{bookmarkLoading?<i className="fa fa-spinner fa-spin"/>:"Bookmark"}</button> ;
+
+  return buttonElement;
+}
+
+export {BookmarkButton}
