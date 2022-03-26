63a06823107bb650d8a10ebc152edd3e8a95a7f9
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-03-26 12:04:26 +0530

    Feat: Add bookmark badge on icon

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,14 @@
 import "./navigation.css";
 import {Link} from "react-router-dom";
 import { useAuth } from "../../contexts/Providers/AuthProvider/AuthProvider";
+import { useBookmarks } from "../../contexts/Providers/BookmarksProvider/BookmarksProvider";
 
 const Navigation=()=>{
 const {authState, authDispatch} = useAuth();
-
+const {bookmarksState} = useBookmarks();
 const {isAuthenticated} = authState;
+
+const {bookmarks} = bookmarksState;
 
 
     return (<nav className="nav-row flex-row">
@@ -19,9 +22,9 @@ const {isAuthenticated} = authState;
     <div className="nav-links links-top flex-row">
         {isAuthenticated?<button className="btn prim-btn btn-round" onClick={()=>authDispatch({type:"LOGOUT"})}>Logout</button>:
         <Link className="nav-link" to="/login"><button className="btn prim-btn btn-round">Login</button></Link> }
-        
         <Link className="nav-link" to="/bookmark"><img className="icon nav-icon"
-                src="https://res.cloudinary.com/carsmart/image/upload/v1647404723/icons/bookmark_chopzg.png"/></Link>
+                src="https://res.cloudinary.com/carsmart/image/upload/v1647404723/icons/bookmark_chopzg.png"/>
+                <p class="badge-text badge-text-m pos-r pos-t">{bookmarks.length}</p></Link>
         <Link className="nav-link" to="/compare"><img className="icon nav-icon"
                 src="https://res.cloudinary.com/carsmart/image/upload/v1647404724/icons/compare_yzogng.png"/></Link>
         <Link className="nav-link" to="/booking"><img className="icon nav-icon" src="https://res.cloudinary.com/carsmart/image/upload/v1647404724/icons/cart_ovd0is.png"/></Link>
