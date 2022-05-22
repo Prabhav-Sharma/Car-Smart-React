71ef67316cec485da1e3f5e932635775996ab511
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-05-23 01:54:32 +0530

    Feat: Add order controller

---
+++ src/backend/controllers/OrderController.js
@@ -0,0 +1,77 @@
+import { Response } from "miragejs";
+import { formatDate, requiresAuth } from "../utils/authUtils";
+
+export const fetchOrdersHandler = function (schema, request) {
+  const userId = requiresAuth.call(this, request);
+  if (!userId) {
+    new Response(
+      404,
+      {},
+      {
+        errors: ["The email you entered is not Registered. Not Found error"],
+      }
+    );
+  }
+  const userOrders = schema.users.findBy({ _id: userId }).orders;
+  return new Response(200, {}, { orders: userOrders });
+};
+
+export const fetchOrderByIdHandler = function (schema, request) {
+  const userId = requiresAuth.call(this, request);
+  try {
+    if (!userId) {
+      new Response(
+        404,
+        {},
+        {
+          errors: ["The email you entered is not Registered. Not Found error"],
+        }
+      );
+    }
+    const orderId = request.params.orderId;
+    const order = schema.users
+      .findBy({ _id: userId })
+      .orders.find((order) => order._id === orderId);
+    return new Response(201, {}, { order: order });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
+
+export const addOrderHandler = function (schema, request) {
+  const userId = requiresAuth.call(this, request);
+  try {
+    if (!userId) {
+      new Response(
+        404,
+        {},
+        {
+          errors: ["The email you entered is not Registered. Not Found error"],
+        }
+      );
+    }
+    const userOrders = schema.users.findBy({ _id: userId }).orders;
+    const { order } = JSON.parse(request.requestBody);
+    userOrders.push({
+      ...order,
+      createdAt: formatDate(),
+      updatedAt: formatDate(),
+    });
+    this.db.users.update({ _id: userId }, { orders: userOrders });
+    return new Response(201, {}, { orders: userOrders });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
