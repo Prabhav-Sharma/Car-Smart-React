f9b924abf098ec0d7318a13c180f3600ce498abe
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-05-23 01:57:01 +0530

    Feat: Add Address component

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,37 +1,66 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CartCard from "../../components/Card/CartCard/CartCard";
-import { fetchCart } from "../../contexts/Providers/UserDataProvider/helpers";
+import {
+  fetchAddressList,
+  fetchCart,
+} from "../../contexts/Providers/UserDataProvider/helpers";
 import { useUserData } from "../../contexts/Providers/UserDataProvider/UserDataProvider";
 import { useDocumentTitle } from "../../hooks/useDocumentTitle";
 import { useNavigate } from "react-router-dom";
 import "./cart.css";
 import CartCheckout from "../../components/CartCheckout/CartCheckout";
 import { useAuth } from "../../contexts/Providers/AuthProvider/AuthProvider";
+import AddressCard from "../../components/Card/AddressCard/AddressCard";
+import AddressModal from "../../components/AddressModal/AddressModal";
 
 function Cart() {
-  const { userDataState, userDataDispatch } = useUserData();
-  const { cart } = userDataState;
-
+  const {
+    userDataState: { cart, addressList },
+    userDataDispatch,
+  } = useUserData();
+  const [showAddressModal, setShowAddressModal] = useState(false);
   const navigate = useNavigate();
   const {
     authState: { token },
   } = useAuth();
 
+  const [currentAddress, setCurrentAddress] = useState({ _id: "" });
+
   useDocumentTitle("Cart | CarSmart");
 
   useEffect(() => {
     fetchCart(token, userDataDispatch);
+    fetchAddressList(token, userDataDispatch);
   }, []);
 
   const cartElement =
     cart.length !== 0 ? (
       <div className="cart-wrapper grid">
         <section className="items-wrapper flex-column">
+          <h5 className="cart-sub-heading">Deliver to:</h5>
+          <div className="address-wrapper flex-row">
+            {addressList.map((address) => (
+              <AddressCard
+                key={address._id}
+                address={address}
+                onClick={() => setCurrentAddress(address)}
+                classes={`${
+                  currentAddress._id === address._id && "selected-address"
+                }`}
+              />
+            ))}
+            <button
+              className="add-address-btn"
+              onClick={() => setShowAddressModal(true)}
+            >
+              Add New Address
+            </button>
+          </div>
           {cart.map((product) => (
             <CartCard key={product._id} prod={product} />
           ))}
         </section>
-        <CartCheckout />
+        <CartCheckout address={currentAddress} />
       </div>
     ) : (
       <section className="items-wrapper flex-column">
@@ -51,6 +80,9 @@ function Cart() {
     <main>
       <h3 className="cart-heading">My Cart</h3>
       {cartElement}
+      <AddressModal
+        showModalState={{ showAddressModal, setShowAddressModal }}
+      />
     </main>
   );
 }
